7f4bcaea882d5bccac321313a2e59f7cb81ed23c
AparnaRada <50664973+AparnaRada@users.noreply.github.com>
2020-07-20 22:02:37 +0530

    Login flow fix (#75)
    * Increased the pagination from 5 to 25
    
    * debug
    
    * debug
    
    * added Dissolving lot
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * test commit
    
    * added reassign retailer
    
    * added unassignDeliverAgent  and Reserve orders
    
    * debug
    
    * debug
    
    * added new key in delivery agent detail
    
    * Added pop up for unassign prepone and dissolving lot
    
    * debug
    
    * Added volume in order summary card
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * added Order Summary
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * fixed issues in cart modification
    
    * debug
    
    * reassign retailer bug fixed
    
    * debug
    
    * added error message
    
    * added error message
    
    * added error message poppup
    
    * debug
    
    * Added dob field in complete order
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * changed column Name
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * integrated all endpoints
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * added new column resolved_by_name
    
    * debug
    
    * debug
    
    * debug
    
    * Added env and getRedirectUrl
    
    * debug
    
    * debug
    
    * removed hipbar domain check in email
    
    Co-authored-by: NishankParameswaran <nishant.parameswaran@hipbar.com>
    Co-authored-by: puja-agrawal <puja.rani@hipbar.com>
    Co-authored-by: Sharadha-Krishna <sharadha.krishna@hipbar.com>
    Co-authored-by: NishankP <nishank.parameswaran@hipbar.com>

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -3,7 +3,7 @@ const otpRegex = /^\d{6}$/
 const mobileNoRegex = /^[6789]\d{9}$/
 //const emailRegex = /^(a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/
 export const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-export const hipbarEmailRegex = /(\W|^)[\w.+\-]*@hipbar\.com(\W|$)/
+// export const hipbarEmailRegex = /(\W|^)[\w.+\-]*@hipbar\.com(\W|$)/
 
 export function validateNumberField({ fieldName, fieldValue }) {
   if (fieldValue && fieldValue.trim().length === 0) {
@@ -40,12 +40,13 @@ export function validateEmail({fieldName, fieldValue}) {
       status: true,
       value: `${fieldName} is invalid`
     }
-  } else if ((fieldName === "Email" && !hipbarEmailRegex.test(fieldValue))) {
-    return {
-      status: true,
-      value: `${fieldName} is invalid`
-    }
-  }
+  } 
+  // else if ((fieldName === "Email" && !hipbarEmailRegex.test(fieldValue))) {
+  //   return {
+  //     status: true,
+  //     value: `${fieldName} is invalid`
+  //   }
+  // }
   return {
     status: false,
     value: ''
